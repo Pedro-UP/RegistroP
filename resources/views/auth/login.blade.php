4016d068cfc344c5ce03e55e19664ae0b954fb3a
Pedro-UP <pedrocazorla03@gmail.com>
2022-04-26 22:06:39 -0500

    Inicio de seccion completo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,48 +1,72 @@
+<style>
+    .img-redonda {
+        width: 200px;
+        height: 200px;
+        border-radius: 150px;
+    }
+
+    .contenedor {
+        padding-top: 30px;
+        padding-bottom: 80px;
+        padding-right: 50px;
+        padding-left: 40px;
+        border: 1px solid black;
+        background-color: azure;
+    }
+
+</style>
 <x-guest-layout>
     <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
+        <section class="contenedor">
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+            <x-slot name="logo">
+                <img src="imagenes/logopc.jfif" class="img-redonda" width="200" height="40" alt="">
+            </x-slot>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+            <x-jet-validation-errors class="mb-4" />
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+            @if (session('status'))
+                <div class="mb-4 font-medium text-sm text-green-600">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
-                    <x-jet-checkbox id="remember_me" name="remember" />
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-                </label>
-            </div>
+                <div>
+                    <x-jet-label for="email" value="{{ __('Email') }}" />
+                    <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                        required autofocus />
+                </div>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+                <div class="mt-4">
+                    <x-jet-label for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="current-password" />
+                </div>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <x-jet-checkbox id="remember_me" name="remember" />
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Recordar contraseña') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                            href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste la contraseña?') }}
+                        </a>
+                    @endif
+
+                    <x-jet-button class="ml-4">
+                        {{ __('INICIAR') }}
+                    </x-jet-button>
+                </div>
+            </form>
+        </section>
     </x-jet-authentication-card>
 </x-guest-layout>
--- resources/views/layouts/plantillabase.blade.php
+++ resources/views/layouts/plantillabase.blade.php
@@ -12,14 +12,25 @@
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>TecNoVenta</title>
+
 </head>
 
 <body>
     <h1 class="bg-warning text-Black text-center">Registro de Articulos Computacionales</h1>
 
+    <header>
+        <nav class="navbar navbar-light" style="background-color: #e3f2fd;" style="width:80%">
+            <a class="nav-item navbar-brand" href="{{ route('dashboard') }}"> INICIO </a>
+            <a class="nav-item nav-link" href="{{ route('celulares.index') }}"> CELULARES </a>
+            <a class="nav-item nav-link" href="{{ route('articulos.index') }}"> LAPTOPS </a>
+            <a class="nav-item nav-link" href="{{ route('componentepcs.index') }}"> COMPONENTES PCS </a>
+        </nav>
+    </header>
+
     <div class="container">
         @yield('contenido')
     </div>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
